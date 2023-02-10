378b1761dc0ec2020531a0fbb93448b974a177c9
ChihabEddine Zitouni <cezitouni@gmail.com>
2023-02-10 17:56:14 +0100

    new update

--- src/main/java/PlatformParameter.java
+++ src/main/java/PlatformParameter.java
@@ -12,8 +12,21 @@ public class PlatformParameter {
 
     public  static ContainerController containerController;
 
+    public static ArrayList<Boolean> alive= new ArrayList<>();
     public static String startTime;
+    public static String startTime2;
 
+    public static String methode(){
 
+        LocalDateTime localDateTime = LocalDateTime.now();
+        if(String.valueOf(localDateTime.getMinute()).length()==1){
+            startTime2=localDateTime.getHour()+":0"+localDateTime.getMinute()+":"+localDateTime.getSecond();
+            return localDateTime.getYear()+"_"+localDateTime.getMonthValue()+"_"+localDateTime.getDayOfMonth()+
+                    "_"+localDateTime.getHour()+"_"+localDateTime.getMinute()+"_"+localDateTime.getSecond();
+        }
 
+        startTime2=localDateTime.getHour()+":"+localDateTime.getMinute()+":"+localDateTime.getSecond();
+        return localDateTime.getYear()+"_"+localDateTime.getMonthValue()+"_"+localDateTime.getDayOfMonth()+
+                "_"+localDateTime.getHour()+"_"+localDateTime.getMinute()+"_"+localDateTime.getSecond();
+    }
 }
